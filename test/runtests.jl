0ef68c39327a9af1abc5d4ba51dd07b31e4da33d
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2023-08-30 16:11:35 +0200

    First release

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,12 @@
-using RadCalNet
+using RadCalNet.Modeling: ModelData, tests
+using Flux: mae
 using Test
+import RadCalNet
 
-PERFORMANCE = 0.0021154503f0
+const MODEL = RadCalNet.getradcalnet(scale = false)
+const DATA = ModelData("$(dirname(@__FILE__))/../data/database.h5")
+const LOSS = 0.0022
 
 @testset "RadCalNet.jl" begin
-    # Write your tests here.
+    @test (tests(DATA, 2_000_000) |> a->mae(MODEL(a[1]), a[2])) <= LOSS
 end
